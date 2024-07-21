61b6dec8f32890d175d853d23c5515b027875de3
ctyna <tynakles@alumnos.cei.es>
2024-07-21 19:16:17 +0200

    res

--- env.js
+++ env.js
@@ -1,5 +1,4 @@
 module.exports = {
-    PORT : process.env.PORT || 3000,
-    MONGO : process.env.MONGO || '127.0.0.1:27017/mater'
+
 }
 
--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { router } = require('./router/router')
-const { PORT, MONGO } = require('./env')
 
-const conectar = async ()=> mongoose.connect(`mongodb://${MONGO}`)
-        .then(()=> console.log('Conectando a Mongoose'))
-        .catch(err => console.log(err.message))
+
+const PORT = process.env.PORT || 3000
+const MONGO = process.env.MONGO || 'mongodb://127.0.0.1:27017/mater'
+
+const conectar = async () => mongoose.connect(MONGO)
+    .then(() => console.log('Conectando a Mongoose'))
+    .catch(err => console.log(err.message))
 
 conectar()
 
@@ -18,22 +21,22 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
-router.all('*' , ( req , res , next )=>{
+router.all('*', (req, res, next) => {
     const err = new Error()
-          err.status     = 404 
-          err.statusText = `No encuentro el Endpoint`
+    err.status = 404
+    err.statusText = `No encuentro el Endpoint`
     next(err)
 })
 
 
-router.use(( err , req , res , next )=>{
-    let { status , statusText } = err
-          status     = status     || 500
-          statusText = statusText || `Error interno de mi API`
-    res.status(status).json({ status , statusText })
+router.use((err, req, res, next) => {
+    let { status, statusText } = err
+    status = status || 500
+    statusText = statusText || `Error interno de mi API`
+    res.status(status).json({ status, statusText })
 })
 
 
@@ -46,4 +49,4 @@ router.use(( err , req , res , next )=>{
 
 
 
-app.listen( PORT , ()=>console.log('Iniciando API'))
+app.listen(PORT, () => console.log('Iniciando API'))
